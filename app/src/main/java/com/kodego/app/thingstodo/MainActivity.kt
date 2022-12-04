81b8a0ebc5e98b4fee317cfd5bd0f77e13c50a08
GinPaul <luginlpp@gmail.com>
2022-12-04 16:29:07 +0800

    Dec 4 2022 commit

---
+++ app/src/main/java/com/kodego/app/thingstodo/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.kodego.app.thingstodo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kodego.app.thingstodo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/kodego/app/thingstodo/ToDoData.kt
@@ -0,0 +1,3 @@
+package com.kodego.app.thingstodo
+
+data class ToDoData(var taskId:String, var task:String)
